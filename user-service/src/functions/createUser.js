0cd39d5180d37f33f0422860db61cf1e35c50894
Koh Meng Wang <mengwangk@gmail.com>
2020-04-01 00:29:37 +0800

    Updated

---
+++ user-service/src/functions/createUser.js
@@ -0,0 +1,30 @@
+'use strict'
+const AWS = require('aws-sdk')
+
+module.exports.createUser = async (event, context) => {
+  const body = JSON.parse(event.body)
+  const username = body.username
+  const password = body.password
+  const newUserParams = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Item: {
+      pk: username,
+      password: password
+    }
+  }
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+    const putResult = await dynamodb.put(newUserParams).promise()
+    return {
+      statusCode: 201,
+      headers: {
+
+      }
+    }
+  } catch (putError) {
+    console.log('There was an error putting the new item')
+    console.log('putError', putError)
+    console.log('newUserParams', newUserParams)
+    return new Error('There was an error putting the new item')
+  }
+}
