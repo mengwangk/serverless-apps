8e79cd9d9e0af3766cd47ea3b4bc35863642888c
Koh Meng Wang <mengwangk@gmail.com>
2020-04-19 23:13:49 +0800

    Updated

--- aws/noteapp/notes-app-client/src/App.js
+++ aws/noteapp/notes-app-client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect bg="light">
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Item href="/signup">Signup</Nav.Item>
+              <Nav.Item href="/login">Login</Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
