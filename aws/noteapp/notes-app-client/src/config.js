ec3c0b526a34099804c19b98bbdd25bb7df52d61
Koh Meng Wang <mengwangk@gmail.com>
2020-04-21 21:11:03 +0800

    Updated

--- aws/noteapp/notes-app-client/src/config.js
+++ aws/noteapp/notes-app-client/src/config.js
@@ -1,16 +1,16 @@
 export default {
     s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+      REGION: "us-east-1",
+      BUCKET: "notes-app-kmw"
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "YOUR_API_GATEWAY_URL"
+      URL: "https://3x0jlqhak7.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_t8otFEUW2",
+      APP_CLIENT_ID: "49pd6a5sd6cnpjbjt9blpbiclc",
+      IDENTITY_POOL_ID: "us-east-1:e90b5c25-9a98-4fcb-acd0-cb4e0fbb2b9d"
     }
   };
