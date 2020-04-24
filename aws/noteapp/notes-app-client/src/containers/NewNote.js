25500593259f84a905d0423ce9cc18ccd20bfa7d
Koh Meng Wang <mengwangk@gmail.com>
2020-04-24 22:38:28 +0800

    Updated

--- aws/noteapp/notes-app-client/src/containers/NewNote.js
+++ aws/noteapp/notes-app-client/src/containers/NewNote.js
@@ -6,6 +6,7 @@ import { onError } from "../libs/errorLib";
 import config from "../config";
 import "./NewNote.css";
 import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 
 export default function NewNote() {
   const file = useRef(null);
@@ -23,29 +24,32 @@ export default function NewNote() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
+        `Please pick a file smaller than ${
+          config.MAX_ATTACHMENT_SIZE / 1000000
+        } MB.`
       );
       return;
     }
-  
+
     setIsLoading(true);
-  
+
     try {
-      await createNote({ content });
+      const attachment = file.current ? await s3Upload(file.current) : null;
+
+      await createNote({ content, attachment });
       history.push("/");
     } catch (e) {
       onError(e);
       setIsLoading(false);
     }
   }
-  
+
   function createNote(note) {
     return API.post("notes", "/notes", {
-      body: note
+      body: note,
     });
   }
 
