dd0137d1b8411379ced6cef6d54a413c92568d6c
Koh Meng Wang <mengwangk@gmail.com>
2020-04-25 09:33:36 +0800

    Updated

--- aws/noteapp/notes-app-client/src/containers/Home.js
+++ aws/noteapp/notes-app-client/src/containers/Home.js
@@ -1,16 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { Card, ListGroup } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import { API } from "aws-amplify";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes");
+  }
+
   function renderNotesList(notes) {
-    return null;
+    console.log(notes[0].content.trim().split("\n")[0]);
+    return [{}].concat(notes).map((note, i) =>
+      i !== 0 ? (
+        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+          <ListGroup.Item action>
+            <Card>
+              <Card.Body>
+                <Card.Title>{note.content.trim().split("\n")[0]}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {"Created: " + new Date(note.createdAt).toLocaleString()}
+                </Card.Subtitle>
+                <Card.Text><br/>{note.content}</Card.Text>
+              </Card.Body>
+            </Card>
+          </ListGroup.Item>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/notes/new">
+          <ListGroup.Item>
+            <h4>
+              <b>{"\uFF0B"}</b> Create a new note
+            </h4>
+          </ListGroup.Item>
+        </LinkContainer>
+      )
+    );
   }
 
   function renderLander() {
