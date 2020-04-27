951a82ec10ef1bdc55e1328b92a92fdf04792e52
Koh Meng Wang <mengwangk@gmail.com>
2020-04-27 23:31:27 +0800

    Updated

--- aws/noteapp/notes-app-api/libs/handler-lib.js
+++ aws/noteapp/notes-app-api/libs/handler-lib.js
@@ -1,12 +1,19 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
-    return function (event, context) {
-      return Promise.resolve()
+  return function (event, context) {
+    return (
+      Promise.resolve()
+        // Start debugger
+        .then(() => debug.init(event, context))
         // Run the Lambda
         .then(() => lambda(event, context))
         // On success
         .then((responseBody) => [200, responseBody])
         // On failure
         .catch((e) => {
+          // Print debug messages
+          debug.flush(e);
           return [500, { error: e.message }];
         })
         // Return HTTP response
@@ -17,6 +24,9 @@ export default function handler(lambda) {
             "Access-Control-Allow-Credentials": true,
           },
           body: JSON.stringify(body),
-        }));
-    };
-  }
+        }))
+        // Cleanup debugger
+        .finally(debug.end)
+    );
+  };
+}
